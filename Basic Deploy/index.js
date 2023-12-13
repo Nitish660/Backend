419ec880b0e3aacf51886a7583e10ac0f9d020ae
Nitish660 <20051742@kiit.ac.in>
2023-12-13 09:49:16 +0530

    commit

---
+++ Basic Deploy/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.send('Hello wolrd!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('nitish660')
+})
+
+app.get('/login',(req,res) =>{
+    res.send('please login')
+})
+
+app.listen(port,()=> {
+    console.log(`Example app listening on port ${port}`)
+})
