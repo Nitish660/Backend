1cc3980be92b53232fae61961cc5988f6b7efc6f
Nitish660 <20051742@kiit.ac.in>
2023-12-16 09:39:05 +0530

    middleware

---
+++ Backend Project/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//const asyncHandler =() =>{}
+//const asynHandler = (func) => () => {}
+//const asyncHandler = (func) =. async() => {}
+
+import { request } from "express"
+
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((err) => next)
+    }
+}
+
+export { asyncHandler }
+
+
+/*const asyncHandler = (fn) => async(res,req,next) =>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.messsage
+        })
+    }
+}
+*/
