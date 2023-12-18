dfbb52773323328009767dfa4bcff1420e71b62e
Nitish660 <20051742@kiit.ac.in>
2023-12-18 22:43:49 +0530

    controller

--- Backend Project/src/app.js
+++ Backend Project/src/app.js
@@ -1,20 +1,27 @@
 import express from "express";
-import cors from "cors"
-import cookieParser from "cookieParser"
+import cors from "cors";
+import cookieParser from "cookie-parser"; // Corrected import statement
 
-const app = express()
+const app = express();
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}));
 
-app.use(express.json({ limit: "16kb" }))
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
-}))
-app.use(express.static("public"))
+}));
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-export { app } 
+// Routes
+import useRouter from "../src/routes/user.routes.js";
+
+// Routes declaration
+app.use("/api/v1/users", useRouter);
+
+export { app };
--- Backend Project/src/utils/asyncHandler.js
+++ Backend Project/src/utils/asyncHandler.js
@@ -6,9 +6,9 @@ import request  from "express"
 
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+  return  (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
-            .catch((err) => next)
+            .catch((err) => next(err))
     }
 }
 
