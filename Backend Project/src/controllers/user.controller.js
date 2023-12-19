19231ea1c13d06ab1a9ee85b9f53ad2ff08906df
Nitish660 <20051742@kiit.ac.in>
2023-12-19 22:01:54 +0530

    upload

--- Backend Project/src/controllers/user.controller.js
+++ Backend Project/src/controllers/user.controller.js
@@ -1,11 +1,41 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary } from "../utils/cloudinary.js"
 
-const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:""
-    }
-    )
-})
+const registerUser = asyncHandler(async (req, res) => {
+  //get user details from frontend
+  //validate if empty or not
+  //check if user already exist
+  //check for images, chec for avatar
+  //upload them to cloudninary
+  //create user object - create entry in db
+  //remove password and refrsh token field from response
+  // check fro user creation
+  // return res
 
-export {registerUser,
-}
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const converImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar file is required");
+  }
+});
+
+export { registerUser };
